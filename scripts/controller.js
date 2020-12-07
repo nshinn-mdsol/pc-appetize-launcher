e61f9aea496ba6cfe2c6d7e8b4e0fdf5da0c4231
Nicholas Shinn <nshinn@mdsol.com>
2020-12-07 12:49:38 -0500

    Update OS versions

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,6 +3,9 @@ angular.module('appetizePages')
 
 	const IOS_VERSION = 'iOS v2020.6.0';
 	const ANDROID_VERSION = 'Android v2020.6.0';
+	
+	const IOS_OS_VERSION = '14.1'; // iOS 14
+	const ANDROID_OS_VERSION = '11.0'; // Android 11
 
 	toastr.options.timeOut = 5000;
     	toastr.options.positionClass = 'toast-top-center';
@@ -60,7 +63,9 @@ angular.module('appetizePages')
 		$scope.apps[$scope.platform][$scope.deviceType] +
 		'?device=' +
 		$scope.devices[$scope.platform][$scope.deviceType] +
-		'&scale=75&deviceColor=white&screenOnly=false&centered=true&xdocMsg=true&osVersion=13.3&debug=true&proxy=intercept&language=' +
+		'&scale=75&deviceColor=white&screenOnly=false&centered=true&xdocMsg=true&osVersion=' +
+		IOS_OS_VERSION +
+		'&debug=true&proxy=intercept&language=' +
 		$scope.currentLangauge;
 
 	$scope.$watch("scale", function(newValue, oldValue) {
@@ -79,12 +84,17 @@ angular.module('appetizePages')
 			$scope[field] = newValue;
 			$scope.sessionActive = false;
 			$scope.currentDevice = $scope.deviceNames[$scope.platform][$scope.deviceType]
+			var currentOSVersion = IOS_OS_VERSION;
 			if ($scope.platform == 'ios') {
 				$scope.currentVersion = IOS_VERSION;
 			} else {
 				$scope.currentVersion = ANDROID_VERSION;
+				currentOSVersion = ANDROID_OS_VERSION;
 			}
-			$scope.logSrc = $scope.apps[$scope.platform][$scope.deviceType] + '?device=' + $scope.devices[$scope.platform][$scope.deviceType] + '&scale=75&deviceColor=white&screenOnly=false&centered=true&xdocMsg=true&osVersion=13.3&debug=true&proxy=intercept&language=' + $scope.currentLangauge;
+			$scope.logSrc = $scope.apps[$scope.platform][$scope.deviceType] + 
+				'?device=' + $scope.devices[$scope.platform][$scope.deviceType] + 
+				'&scale=75&deviceColor=white&screenOnly=false&centered=true&xdocMsg=true&osVersion=' + currentOSVersion +
+				'&debug=true&proxy=intercept&language=' + $scope.currentLangauge;
 		}
 		toastr.remove();
 	}
